8ef1f7ab54a6e6f4648bc344a316282cb4cb650c
yonatanvolog <yonatan.vologdin@gmail.com>
2024-12-01 03:44:04 +0200

    Finised c and d (score given for enemy destruction)

--- Assets/Scripts/LifeTotalScript.cs
+++ Assets/Scripts/LifeTotalScript.cs
@@ -4,10 +4,12 @@ public class LifeTotalScript : MonoBehaviour
 {
     [SerializeField] private float initialLifeTotal = 100f;
     private float lifeTotal;
+    private ScoreScript scoreScript;
 
     private void Start()
     {
         lifeTotal = initialLifeTotal;
+        scoreScript = FindObjectOfType<ScoreScript>(); // player's score
     }
 
     public float GetInitialLifeTotal()
@@ -20,9 +22,17 @@ public class LifeTotalScript : MonoBehaviour
         lifeTotal -= damage;
         if (lifeTotal <= 0)
         {
+            if (CompareTag("Monster"))
+            {
+                if (scoreScript != null)
+                {
+                    scoreScript.AddScore();
+                }
+            }
             Destroy(gameObject);
         }
     }
+
 
     public void Heal(float healAmount)
     {
